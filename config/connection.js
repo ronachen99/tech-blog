659a477361876b2b09da0a1b9432fa720361ae3e
Rona C <rona.chen@hotmail.com>
2023-06-12 18:23:43 -0400

    updated with comments

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+// Declare a variable for sequelize
 let sequelize;
 
+// Set up environment for the application
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
