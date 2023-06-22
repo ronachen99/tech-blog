fe8c732746c6ba9517c700e16d2a97d866932b60
Rona C <rona.chen@hotmail.com>
2023-06-22 02:24:30 -0400

    added a code to make sure the pw are hased properly

--- seeds/userData.js
+++ seeds/userData.js
@@ -11,5 +11,9 @@ const userData = [
   }
 ];
 
-const seedUser = () => User.bulkCreate(userData);
+const seedUser = () =>
+  User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true
+  });
 module.exports = seedUser;
